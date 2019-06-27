74d003b15c12e7d9ef7bf01a86ce654e4fe82176
wangpd3 <wangpd3@asiainfo-sec.com>
2019-06-27 11:19:21 +0800

    first commit

---
+++ src/main/java/wang/peidun/mhstudio/MhStudioApplication.java
@@ -0,0 +1,13 @@
+package wang.peidun.mhstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MhStudioApplication.class, args);
+    }
+
+}
