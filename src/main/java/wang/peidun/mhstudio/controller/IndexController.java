8ec1dd3087b2396cbcd5edbc4d3f450588ebe9b1
wangpd3 <wangpd3@asiainfo-sec.com>
2019-07-01 21:31:29 +0800

    home page

---
+++ src/main/java/wang/peidun/mhstudio/controller/IndexController.java
@@ -0,0 +1,16 @@
+package wang.peidun.mhstudio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: wangpd
+ * @Date: 2019-06-27 15:04
+ */
+@Controller
+public class IndexController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+}
